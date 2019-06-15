089bea22372a432b35e132b6a29fcaa18b320782
arun <aj@accubits.com>
2019-06-15 06:40:28 +0000

    Student Schema Added

---
+++ docs/Samples/student.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `student` (
+    `id` character varying(255) NOT NULL,
+    `first_name` character varying(255),
+    `last_name` character varying(255),
+    `age` integer,
+    `marks` integer,
+    PRIMARY KEY (`id`)
+);
+
